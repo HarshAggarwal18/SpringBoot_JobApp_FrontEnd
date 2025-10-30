a69390f1ac43142bc680e8e2ed56c5fe545771e7
Harsh Aggarwal <harshgrwl18@gmail.com>
2025-10-30 18:58:34 +0530

    Authentiacation Message updated

--- src/Services/AuthService.tsx
+++ src/Services/AuthService.tsx
@@ -1,9 +1,12 @@
-import axios from 'axios';
-const base_url = "http://localhost:8080/auth/"
-const loginUser = async (login:any)=> {
-    return axios.post(`${base_url}login`, login)
-        .then((result:any) => result.data)
-        .catch((error:any) =>{throw error;});
-}
+import axios from "axios";
+const base_url = "https://springboot-jobapp-backend.onrender.com/";
+const loginUser = async (login: any) => {
+  return axios
+    .post(`${base_url}login`, login)
+    .then((result: any) => result.data)
+    .catch((error: any) => {
+      throw error;
+    });
+};
 
-export {loginUser};
+export { loginUser };
