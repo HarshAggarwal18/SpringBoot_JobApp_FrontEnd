0d997b9222cc073d39079f53a2eecc9753db925e
Harsh Aggarwal <harshgrwl18@gmail.com>
2025-10-30 19:50:10 +0530

    axios Instacnec , axios main both baseurl changed

--- src/Services/AuthService.tsx
+++ src/Services/AuthService.tsx
@@ -1,12 +1,15 @@
 import axios from "axios";
-const base_url = "https://springboot-jobapp-backend.onrender.com/";
+const base_url = "https://springboot-jobapp-backend.onrender.com/auth/";
 const loginUser = async (login: any) => {
-  return axios
-    .post(`${base_url}login`, login)
-    .then((result: any) => result.data)
-    .catch((error: any) => {
-      throw error;
-    });
+  return (
+    axios
+      // .post(`${base_url}login`, login)
+      .post(`${base_url}login`, login)
+      .then((result: any) => result.data)
+      .catch((error: any) => {
+        throw error;
+      })
+  );
 };
 
 export { loginUser };
--- src/Services/UserService.tsx
+++ src/Services/UserService.tsx
@@ -1,32 +1,51 @@
 import axiosInstance from "../Interceptor/AxiosInterceptor";
 
+const registerUser = async (user: any) => {
+  return axiosInstance
+    .post(`/users/register`, user)
+    .then((result: any) => {
+      result.data;
+      //   console.log(result.config.url);
+      console.log("url result -->" + result.config?.url);
+    })
+    .catch((error: any) => {
+      console.log("url -->" + error.config?.url);
+      throw error;
+    });
+};
+const loginUser = async (login: any) => {
+  return axiosInstance
+    .post(`/users/login`, login)
+    .then((result: any) => result.data)
+    .catch((error: any) => {
+      throw error;
+    });
+};
+const sendOtp = async (email: string) => {
+  return axiosInstance
+    .post(`/users/sendOtp/${email}`)
+    .then((result: any) => result.data)
+    .catch((error: any) => {
+      throw error;
+    });
+};
 
-const registerUser = async (user:any)=> {
-    return axiosInstance.post(`/users/register`, user)
-        .then((result:any) => result.data)
-        .catch((error:any) =>{throw error;});
-}
-const loginUser = async (login:any)=> {
-    return axiosInstance.post(`/users/login`, login)
-        .then((result:any) => result.data)
-        .catch((error:any) =>{throw error;});
-}
-const sendOtp=async (email:string)=>{
-    return axiosInstance.post(`/users/sendOtp/${email}`)
-        .then((result:any) => result.data)
-        .catch((error:any) =>{throw error;});
-}
+const verifyOtp = async (email: string, otp: string) => {
+  return axiosInstance
+    .get(`/users/verifyOtp/${email}/${otp}`)
+    .then((result: any) => result.data)
+    .catch((error: any) => {
+      throw error;
+    });
+};
 
-const verifyOtp=async (email:string, otp:string)=>{
-    return axiosInstance.get(`/users/verifyOtp/${email}/${otp}`)
-        .then((result:any) => result.data)
-        .catch((error:any) =>{throw error;});
-}
+const resetPassword = async (email: string, password: string) => {
+  return axiosInstance
+    .post(`/users/changePass`, { email, password })
+    .then((result: any) => result.data)
+    .catch((error: any) => {
+      throw error;
+    });
+};
 
-const resetPassword=async (email:string, password:string)=>{
-    return axiosInstance.post(`/users/changePass`, {email, password})
-        .then((result:any) => result.data)
-        .catch((error:any) =>{throw error;});
-}
-
-export {registerUser, loginUser, sendOtp, verifyOtp, resetPassword};
+export { registerUser, loginUser, sendOtp, verifyOtp, resetPassword };
